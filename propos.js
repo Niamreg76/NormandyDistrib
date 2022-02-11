0522ceba35a912c4ff6c0075f4d981184853c562
Niamreg76 <84070314+Niamreg76@users.noreply.github.com>
2022-02-11 15:40:07 +0100

    Update propos.js

--- propos.js
+++ propos.js
@@ -14,7 +14,7 @@ const hero = document.querySelector('.services');
       
           const xWalk = Math.round((x / width * walk) - (walk / 2));
           const yWalk = Math.round((y / height * walk) - (walk / 2));
-      
+        
           text.style.textShadow = `${xWalk}px ${yWalk}px 0 blue`;
       
         }
